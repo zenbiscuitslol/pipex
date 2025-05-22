986dfc263e491793765730179b085b8e6ccf90ae
Pranin Edward Prejith <pprejith@3-E-23.42heilbronn.de>
2025-05-22 17:05:24 +0200

    pipex start

---
+++ src/pipex.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pprejith <pprejith@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/22 16:08:54 by pprejith          #+#    #+#             */
+/*   Updated: 2025/05/22 16:28:51 by pprejith         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void pipex(char **argv, char **envp)
+{
+	
+}
+
+
+int	main(int argc, char **argv, char **envp)
+{
+	if (argc <= 4)
+		ft_printf("Not enough arguments!");
+}
---
+++ src/pipex.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pprejith <pprejith@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/22 16:09:54 by pprejith          #+#    #+#             */
+/*   Updated: 2025/05/22 16:12:30 by pprejith         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <errno.h>
+# include <string.h>
+
+#endif
